ae7c5e46135a0395ffc61385dfb10f98aa376981
kucengorenji <mikelrisyad@gmail.com>
2021-07-08 15:11:52 +0700

    first try to render the api into the web page using fetch

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const submitButton = document.getElementById("submit-button");
+const resultBox = document.getElementById("box");
+
+apiUrl = "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
 
 submitButton.addEventListener("click", function() {
     alert("aloha");
 })
+
+const returnData = (data) => {
+    return resultBox.innerHTML = `
+    <img src="${data.album.images[0].url}" alt="">
+    <p>Track Title : ${data.name}</p>
+    <p>Artist : ${data.artists.name}</p>
+    <p>Album : ${data.album}</p>
+    <button id="submit-button" type="submit">Select</button>
+    `
+}
+
+fetch(apiUrl)
+      .then((response) => response.json())
+      
+    //   .then(returnData(data))
+      .then((data) => returnData(data));
+
